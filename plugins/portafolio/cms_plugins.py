6b4f279a3f43d19ce1f1b14a8ce6fcb771e21283
Andres Vargas <andres.vargas@interalia.net>
2010-12-27 13:36:39 -0600

    adding more ignore fixing js errors and plugin css

--- plugins/portafolio/cms_plugins.py
+++ plugins/portafolio/cms_plugins.py
@@ -1,11 +1,11 @@
 from models import PortafolioPlugin
 from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
+from django.conf import settings 
 class PortafolioPluginBase(CMSPluginBase):
     model = PortafolioPlugin
     render_template = 'portafolio/portafolio.html'
-    name = "Portafolio"
-
+    name = u"Portafolio"
     def render(self, context, instance, placeholder):
         context.update({'instance':instance,'placeholder':placeholder})
         return context
