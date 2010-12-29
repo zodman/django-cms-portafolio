2742891acf97135e340d37bcb1d51d5e0a4a6419
andres.vargas <andres.vargas@e052fa42-3820-444b-a986-7287d7577163>
2010-12-29 23:19:55 +0000

    change portafolio structure
    git-svn-id: http://svn.net.interalia.net/desarrolloweb/interaliaweb/trunk@18 e052fa42-3820-444b-a986-7287d7577163

---
+++ portafolio/templatetags/portafolio_menu.py
@@ -0,0 +1,13 @@
+from django import template
+from cms
+
+register = template.Library()
+
+class PortafolioMenuNode(template.Node):
+    def render(self, context):
+        context["portafolio_clients"] = 
+        
+
+class portafolio_menu_client(parse, token):
+    pass
+    
