edfe829514ab9f812ab43a125155635330a38426
andres.vargas <andres.vargas@e052fa42-3820-444b-a986-7287d7577163>
2010-12-30 00:28:34 +0000

    cahnages applied
    git-svn-id: http://svn.net.interalia.net/desarrolloweb/interaliaweb/trunk@30 e052fa42-3820-444b-a986-7287d7577163

--- portafolio/urls.py
+++ portafolio/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('portafolio.views',
-    url(r"^(P?<slug>[-\w]*)*$", "index", name="portafolio-index"),
-
+    url(r"^$", "index", name="portafolio-index"),
+    url(r"^(?P<slug>[-\w]+)$", "proyect", name="portafolio-proyect"),
 )
 
--- portafolio/views.py
+++ portafolio/views.py
@@ -3,11 +3,16 @@ from django.views.generic.simple import direct_to_template
 from django.shortcuts import get_object_or_404
 from models import Proyect
 
-def index(request, slug = None):
-    if slug:
-        p = get_object_or_404(Proyect, slug = slug)
-    else:
-        p =  Proyect.objects.all()[0]
+
+
+def index(request):
+    p = Proyect.objects.all()[0]
+    return direct_to_template(request, "portafolio/index.html", 
+                              extra_context = { 'proyect':p })
+
+
+def proyect(request, slug):
+    p = get_object_or_404(Proyect, slug = slug)
     return direct_to_template(request, "portafolio/index.html", 
                               extra_context = { 'proyect':p })
 
