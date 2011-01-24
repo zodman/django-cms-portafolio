e6a79dc8423761c9f894a3c193f83c05c2285a2f
andres.vargas <andres.vargas@e052fa42-3820-444b-a986-7287d7577163>
2011-01-24 23:45:44 +0000

    agregando un editor a proyectos
    git-svn-id: http://svn.net.interalia.net/desarrolloweb/interaliaweb/trunk@71 e052fa42-3820-444b-a986-7287d7577163

--- portafolio/admin.py
+++ portafolio/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
+    
+from django.db import models
+from django.contrib import admin
+
 from models import Service, Client, Proyect, Image
+
+from cms.plugins.text.widgets.wymeditor_widget import WYMEditor
 
 class ImageInline(admin.TabularInline):
     model =Image
 
 class ProyectAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
+    formfield_overrides = {
+        models.TextField : {'widget': WYMEditor},
+    }
+    class Media:
+        js = ("js/jquery-1.4.3.min.js",)
 
 
 admin.site.register(Service)
