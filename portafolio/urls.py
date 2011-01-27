73c07efc8d35cce09af4fb02bf0601f150fe8e80
andres.vargas <andres.vargas@e052fa42-3820-444b-a986-7287d7577163>
2011-01-27 23:53:10 +0000

    fix typo on url
    git-svn-id: http://svn.net.interalia.net/desarrolloweb/interaliaweb/trunk@79 e052fa42-3820-444b-a986-7287d7577163

--- portafolio/urls.py
+++ portafolio/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('portafolio.views',
     url(r"^$", "index", name="portafolio-index"),
-    url(r"^(?P<slug>[-\w]+)/$", "proyect", name="portafolio-proyect"),
-    url(r"^redirect/$", "redirect", name="portafolio-redirect"),
     url(r"^small/$", "show_small", name="portafolio-small"),
+    url(r"^redirect/$", "redirect", name="portafolio-redirect"),
+    url(r"^detail/(?P<slug>[-\w]+)/$", "proyect", name="portafolio-proyect"),
 )
 
