7e1369f80bb34ff950550b90f959628010efacc4
andres.vargas <andres.vargas@e052fa42-3820-444b-a986-7287d7577163>
2011-01-27 23:47:55 +0000

    fix typo on url
    git-svn-id: http://svn.net.interalia.net/desarrolloweb/interaliaweb/trunk@78 e052fa42-3820-444b-a986-7287d7577163

--- portafolio/urls.py
+++ portafolio/urls.py
@@ -4,6 +4,6 @@ urlpatterns = patterns('portafolio.views',
     url(r"^$", "index", name="portafolio-index"),
     url(r"^(?P<slug>[-\w]+)/$", "proyect", name="portafolio-proyect"),
     url(r"^redirect/$", "redirect", name="portafolio-redirect"),
-    url(r"small/$", "show_small", name="portafolio-small"),
+    url(r"^small/$", "show_small", name="portafolio-small"),
 )
 
